17eee353a5de744690902c0ab861d5cf3d10c132
Charles-CarM <103493003+Charles-CarM@users.noreply.github.com>
2022-04-19 19:44:10 -0700

    third

--- clock.js
+++ clock.js
@@ -1,16 +1,32 @@
+setInterval(launchTime, 1000);
 
+function launchTime() {
+  let d = new Date();
+  var now = document.getElementById("now");
+  var hours = ('0' + d.getHours() % 12 || 12).slice(-2);
+  var seconds = ('0' + d.getSeconds()).slice(-2);
+  var ampm = hours >= 12 ? "p.m." : "a.m.";
+  var time = (hours + ":" + ('0' + d.getMinutes()).slice(-2) + ":" + seconds + " " + ampm);
+      now.innerHTML = time;
+    }
+
+
+const today = new Date();
+ 
+const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+let day = days[today.getDay()];
+
+const months = ["January", "February", "March", "April", "May", "June",
+                 "July", "August", "September", "October", "November", "December"];
+let month = months[today.getMonth()];
+
+let dayOf = today.getDate();
+let year = today.getFullYear();
 
 var current = document.getElementById("current");
-const today = new Date();
-var date = today.getDay()+'-'+(today.getMonth()+1)+'-'+today.getDate()+'-'+today.getFullYear();
+var date = (day + ", " + month + " " + dayOf + " " + year);  
 current.innerHTML = date;
 
-var now = document.getElementById("now");
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-now.innerHTML = time;
-
-
-setInterval(date, 5000);
 
 
 
